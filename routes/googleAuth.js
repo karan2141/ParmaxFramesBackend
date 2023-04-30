27ffe1c4e7f7cf6f139d5189bc4fcafd180ac87b
Karanveer Singh <karanveer.s@hdfclife.com>
2023-04-30 15:42:14 +0530

    fix

--- routes/googleAuth.js
+++ routes/googleAuth.js
@@ -13,31 +13,36 @@ googleRouter.post(
             console.log(JSON.stringify(req.body));
             if(!data.email_verified) throw "Email not verified"
             console.log('here 1');
-            const user = await User.findOne({ email })
+            const user = await User.findOneAndUpdate({ email }, {googleData: data}, {
+                new: true,
+                upsert: true
+            })
             console.log('here 2');
-            let token = ''
-            if(!user) {
-                const newUser = new User({
-                    email,
-                    googleData: data
-                })
-                await newUser.save()
-                console.log('here 3');
-                token = Jwt.sign({ userId: newUser._id }, JwtSecret, {
-                    expiresIn: JwtExpireInMin*60
-                });
-                console.log('here 4');
-            } else {
-                console.log('here 5');
-                user.googleData = data
-                await user.save()
-                console.log('here 6');
-                token = Jwt.sign({ userId: user._id }, JwtSecret, {
-                    expiresIn: JwtExpireInMin*60
-                });
-                console.log('here 7');
-            }
-            console.log(JSON.stringify(data));
+            const token = Jwt.sign({ userId: newUser._id }, JwtSecret, {
+                expiresIn: JwtExpireInMin*60
+            });
+            // if(!user) {
+            //     const newUser = new User({
+            //         email,
+            //         googleData: data
+            //     })
+            //     await newUser.save()
+            //     console.log('here 3');
+            //     token = Jwt.sign({ userId: newUser._id }, JwtSecret, {
+            //         expiresIn: JwtExpireInMin*60
+            //     });
+            //     console.log('here 4');
+            // } else {
+            //     console.log('here 5');
+            //     user.googleData = data
+            //     await user.save()
+            //     console.log('here 6', JSON.stringify());
+            //     token = Jwt.sign({ userId: user._id }, JwtSecret, {
+            //         expiresIn: JwtExpireInMin*60
+            //     });
+            //     console.log('here 7');
+            // }
+            // console.log(JSON.stringify(data));
             res.send({
                 status: 200,
                 message: 'Otp verified successfully',
