227a1eb6066faa0266b1be762b925b7350903a5e
Karanveer Singh <karanveer.s@hdfclife.com>
2023-07-02 13:36:22 +0530

    fix in contact us mail

--- emailService.js
+++ emailService.js
@@ -155,9 +155,9 @@ export const sendOrderMailToOwner = (params) => {
   }
 };
 
-export const sendContactUsEmail = (params) => {
+export const sendContactUsEmail = async(params) => {
   try {
-    transporter.sendMail({
+    await transporter.sendMail({
       from: MAIL_SETTINGS.auth.user,
       to: MAIL_SETTINGS.auth.user,
       subject: `${params.subject}`,
