a56e85b5da4027c7da456350f6e9286e1f2e2356
Karanveer Sing <kvs2141@gmail.com>
2025-01-08 20:19:38 +0530

    indentation fix

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ import express from 'express'
 import connectMongoDb from './mongoConfig.js'
 import bodyParser from 'body-parser'
 import chalk from 'chalk'
-import cors from 'cors';
+import cors from 'cors'
 import googleRouter from './routes/googleAuth.js'
 import emailAuthRouter from './routes/emailAuth.js'
-import facebookRouter from './routes/facebookAuth.js';
-import baseRouter from './routes/baseRoutes.js';
-import { RAZORPAY_KEY_ID, RAZORPAY_KEY_SECRET } from './constants.js';
-import paymentRouter from './routes/paymentRoute.js';
+import facebookRouter from './routes/facebookAuth.js'
+import baseRouter from './routes/baseRoutes.js'
+import { RAZORPAY_KEY_ID, RAZORPAY_KEY_SECRET } from './constants.js'
+import paymentRouter from './routes/paymentRoute.js'
 import Razorpay from 'razorpay'
-import DecryptRequest from './middleware/decryptReq.js';
-import adminRouter from './routes/adminRoute.js';
+import DecryptRequest from './middleware/decryptReq.js'
+import adminRouter from './routes/adminRoute.js'
 
 connectMongoDb()
 
@@ -25,20 +25,20 @@ app.use(cors({
 app.use(bodyParser.urlencoded({
     extended: true,
     limit: '50mb'
-}));
+}))
 
 // Configure body-parser middleware with increased limit
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json({ limit: '50mb' }))
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
 
-app.use(express.json());
+app.use(express.json())
 
 export const instance = new Razorpay({
     key_id: RAZORPAY_KEY_ID,
     key_secret: RAZORPAY_KEY_SECRET,
-});
+})
 
-app.use("/payment", DecryptRequest, paymentRouter);
+app.use("/payment", DecryptRequest, paymentRouter)
 app.use("/auth", DecryptRequest, googleRouter)
 app.use("/auth", DecryptRequest, facebookRouter)
 app.use("/auth", DecryptRequest, emailAuthRouter)
@@ -46,10 +46,10 @@ app.use("/admin", DecryptRequest, adminRouter)
 app.use("", DecryptRequest, baseRouter)
 
 const port = process.env.PORT || 8080
-app.listen(port, ()=>{
-    console.log(chalk.green(`server started on port ${port}`));
+app.listen(port, () => {
+    console.log(chalk.green(`server started on port ${port}`))
 })
 
 export default (req, res) => {
-    app(req, res); // Call the Express app as a function
-  };
+    app(req, res)
+}
